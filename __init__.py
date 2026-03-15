a40e7203abcd531b1c1c43f5a9fcfeb094b70f45
Adam <phazei@gmail.com>
2026-03-14 23:04:58 -0700

    Surpress warnings

--- __init__.py
+++ __init__.py
@@ -3,7 +3,11 @@
 # This line makes the 'hunyuanvideo_foley' sub-directory importable by your nodes.py
 import sys
 import os
+import warnings
+from pydantic.warnings import UnsupportedFieldAttributeWarning
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+warnings.filterwarnings("ignore", category=UnsupportedFieldAttributeWarning)
 
 # This line imports the node mappings from your nodes.py file
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
